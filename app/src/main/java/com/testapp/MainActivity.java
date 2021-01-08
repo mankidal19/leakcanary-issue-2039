cb603cd4908cb595caf14440ffbe4ca53c3fce3d
Aiman Desktop PC <nurulaiman.abmajid@sony.com>
2021-01-08 17:45:24 +0800

    first commit

---
+++ app/src/main/java/com/testapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.testapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+}
